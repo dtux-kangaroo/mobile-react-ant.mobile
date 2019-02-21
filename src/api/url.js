eedba4408d596ee5aa71291bb564e8a56f0cc19c
nankaNULL <534129087@qq.com>
2019-02-21 16:46:29 +0800

    api修改

---
+++ src/api/url.js
@@ -0,0 +1,13 @@
+export default {
+  //登录
+  login:{
+    method:'post',
+    url:'/api/distr/login'
+  },
+  // getHomeInfo:'',
+  // getNavList:'./mock/nav.json',
+  // getExceptList:``,
+  // getSearchList:``,
+  // reqLogin:``,
+  // getUserInfo:``,
+}
