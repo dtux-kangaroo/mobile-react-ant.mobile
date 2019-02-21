678347fd75544a1d5d73e62ebf01cb3b0d1b42e7
Erindcl <cailian_erin@163.com>
2019-02-21 17:19:08 +0800

    整理文件（src/global/action.js中仍使用旧方法请求数据，待修改

---
+++ src/constants/apiUrl.js
@@ -0,0 +1,8 @@
+export default {
+    getHomeInfo:'',
+    getNavList:'./mock/nav.json',
+    getExceptList:``,
+    getSearchList:``,
+    reqLogin:``,
+    getUserInfo:``,
+}
--- src/pages/global/action.js
+++ src/pages/global/action.js
@@ -1,5 +1,7 @@
 import {globalType} from './constant';
-import { API } from "@/api/index.js";
+// import { API } from "@/api/index.js"; // 引入会报错：ReferenceError: regeneratorRuntime is not defined
+import http from '../../utils/http'
+import apiUrl from 'constants/apiUrl';
 
 const navData = (data) => ({
   type: globalType.GET_NAV_DATA,
@@ -7,13 +9,19 @@ const navData = (data) => ({
 })
 export const getNavData = (params) => async (dispatch, getState) => {
   try {
-    API.getNavList(params).then(response =>{ 
-      if (response.success) {
-        dispatch(navData(response.data));
-      } else {
+    let response = await http.get(apiUrl.getNavList, params);
+    if (response.success) {
+        await dispatch(navData(response.data));
+    } else {
         //返回失败
-      }
-    });
+    }
+    // API.getNavList(params).then(response =>{ 
+    //   if (response.success) {
+    //     dispatch(navData(response.data));
+    //   } else {
+    //     //返回失败
+    //   }
+    // }); 
   } catch (error) {
     console.log('error: ', error)
   }
